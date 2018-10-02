dac83f14f310866c2f2b10801c20b2cf0937bfd8
abstract-animation-9914 <vm930@hotmail.com>
2018-10-02 00:32:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 /* Enter the code to remove the main node element under this comment */
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+// const newHeader = document.createElement('div');
+// document.body.appendChild(newHeader);
+
+// let itemsToDelete = document.querySelector("#main")
+// ul.remove();
+
+// newHeader = document.createElement("h1")
+// document.getElementsByTagName("h1")[0].setAttribute("id","victory")
+// newHeader.innerText = "Vickysdailystandup is the champion"
+// body.appendChild(newHeader);
+
+let main = document.querySelector("#main");
+main.remove();
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+newHeader.innerText = "victoria is the champion";
+body.appendChild(newHeader);
